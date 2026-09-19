bea79cea89456c8288c1137ae56a14b2e251a3ef
CheeyaStudio <mayalestari0805@gmail.com>
2026-09-19 11:43:30 +0700

    Clean up search bar UI and bump cache v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// Service Worker for CheeYa Studio • German A1 PWA
-const CACHE_NAME = 'cheeya-german-a1-v8';
+const CACHE_NAME = 'cheeya-german-a1-v9';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
