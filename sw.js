8e835a77a52dc1ecec24d9e68ab8ce1b3ee2bfc4
CheeyaStudio <mayalestari0805@gmail.com>
2026-09-19 11:38:36 +0700

    Add Domain Lock anti-theft guard and polish UI

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// Service Worker for CheeYa Studio • German A1 PWA
-const CACHE_NAME = 'cheeya-german-a1-v7';
+const CACHE_NAME = 'cheeya-german-a1-v8';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
