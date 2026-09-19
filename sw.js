f9c233daacee2e1b9c54729938992cab68772d90
CheeyaStudio <mayalestari0805@gmail.com>
2026-09-19 11:17:21 +0700

    Add smart content protection layer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker for CheeYa Studio • German A1 PWA
-const CACHE_NAME = 'cheeya-german-a1-v4';
+﻿// Service Worker for CheeYa Studio • German A1 PWA
+const CACHE_NAME = 'cheeya-german-a1-v5';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
