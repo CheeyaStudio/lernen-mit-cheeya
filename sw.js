1b67fd59212accd68ee8a4bd1b9e1ba978992e60
CheeyaStudio <mayalestari0805@gmail.com>
2026-09-20 19:34:24 +0700

    update smart translator

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// Service Worker for CheeYa Studio • German A1 PWA
-const CACHE_NAME = 'cheeya-german-a1-v15';
+const CACHE_NAME = 'cheeya-german-a1-v16';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
