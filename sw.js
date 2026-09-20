6be7a28055fac90b566ecbc1874588b2dfa10e25
CheeyaStudio <mayalestari0805@gmail.com>
2026-09-20 19:55:25 +0700

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// Service Worker for CheeYa Studio • German A1 PWA
-const CACHE_NAME = 'cheeya-german-a1-v16';
+const CACHE_NAME = 'cheeya-german-a1-v17';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
